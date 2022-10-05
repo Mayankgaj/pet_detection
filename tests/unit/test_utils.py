e9b39ae2c24b96d362d93c911552161bc9537432
Mayank Gajbhiye <mayank1398@gmail.com>
2022-10-05 17:30:47 +0530

    test: test cases added

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -2,14 +2,25 @@ import pytest
 from cnn_classifier.utils import read_yaml
 from pathlib import Path
 from box import ConfigBox
-
-yaml_files = [
-    "tests/unit/data/empty.yaml",
-    "tests/unit/data/demo.yaml"
-]
+from ensure.main import EnsureError
 
 
-def test_read_yaml_empty():
-    with pytest.raises(ValueError):
-        read_yaml(Path(yaml_files[0]))
-        
+class Test_read_yaml:
+    yaml_files = [
+        "tests/unit/data/empty.yaml",
+        "tests/unit/data/demo.yaml"
+    ]
+    
+    def test_read_yaml_empty(self):
+        with pytest.raises(ValueError):
+            read_yaml(Path(self.yaml_files[0]))
+            
+    def test_read_yaml_content(self):
+        response = read_yaml(Path(self.yaml_files[1]))
+        assert isinstance(response, ConfigBox) # assert tells if condition is true or false
+
+    @pytest.mark.parametrize("path_to_yaml", yaml_files)    
+    def test_read_yaml_bad_type(self, path_to_yaml):
+        with pytest.raises(EnsureError):
+            read_yaml(path_to_yaml)
+            
