9b171aae4efb5b8ef7fa15bdefdef7a980ece8a9
Mayank Gajbhiye <mayank1398@gmail.com>
2022-10-26 18:16:16 +0530

    dvc stage 04

--- src/cnn_classifier/pipeline/stage_04_evaluation.py
+++ src/cnn_classifier/pipeline/stage_04_evaluation.py
@@ -1,10 +1,12 @@
 from cnn_classifier.config import ConfigurationManager
 from cnn_classifier.components import Evaluation
 from cnn_classifier import logger
+import os
+os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 import tensorflow as tf
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#tf.config.experimental.set_memory_growth(gpus[0], True)
 
 STAGE_NAME = "Evaluation stage"
 
